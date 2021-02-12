6397074578e794ba4b84841538eba7fa60bc3c87
pontsuyo <tsuyopon9221@gmail.com>
2021-02-12 21:40:03 +0900

    :+1: Tweetのフィールドを用意

--- src/main/java/com/pontsuyo/tweet/analyzer/db/api/domain/model/Tweet.java
+++ src/main/java/com/pontsuyo/tweet/analyzer/db/api/domain/model/Tweet.java
@@ -1,9 +1,21 @@
 package com.pontsuyo.tweet.analyzer.db.api.domain.model;
 
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
 public class Tweet implements Serializable {
-  private String tweetId;
+  private final Long tweetId;
+
+  private final Long userId;
+
+  private final String text;
+
+  private final Integer favoriteCount;
+
+  private final Integer retweetCount;
+
+  private final List<String> imageUrls;
+
 }
