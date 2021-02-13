2bf34e01223bcb53f65320442c37bfa859339bb6
pontsuyo <tsuyopon9221@gmail.com>
2021-02-13 16:05:30 +0900

    :+1: AWS lambda用にhandlerを追加

---
+++ src/main/java/com/pontsuyo/tweet/analyzer/db/api/EventHandler.java
@@ -0,0 +1,6 @@
+package com.pontsuyo.tweet.analyzer.db.api;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class EventHandler extends SpringBootRequestHandler<String, Object> {
+}
