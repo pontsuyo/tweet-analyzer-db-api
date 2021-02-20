648cd078b833028487203b81a86fe79b28f57750
pontsuyo <tsuyopon9221@gmail.com>
2021-02-20 10:08:15 +0900

    :recycle: remove unused interface

--- src/main/java/com/pontsuyo/tweet/analyzer/fetcher/domain/repository/TweetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pontsuyo.tweet.analyzer.fetcher.domain.repository;
-
-
-import com.pontsuyo.tweet.analyzer.fetcher.domain.model.Tweet;
-import java.util.List;
-
-public interface TweetRepository {
-  List<Tweet> getTweets();
-}
